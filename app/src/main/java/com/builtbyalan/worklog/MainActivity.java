e3ebfbd188ca9a476fcb8932575523bbac51f9db
Alan Perez <alanipce@gmail.com>
2018-10-25 15:05:11 -0700

    Integrated firebase into the project to serve as data storage modal.

--- app/src/main/java/com/builtbyalan/worklog/MainActivity.java
+++ app/src/main/java/com/builtbyalan/worklog/MainActivity.java
@@ -1,16 +1,33 @@
 package com.builtbyalan.worklog;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.LayoutManager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private String[] projects;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private List<String> mProjects;
+    private ProjectValueEventListener mOnProjectsChangeListener = new ProjectValueEventListener();
+
+    private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference mProjectsRef = mRootRef.child("projects");
 
     private RecyclerView mRecyclerView;
     private LayoutManager mLayoutManager;
@@ -22,30 +39,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        projects = new String[] {"CaseAide® Note", "Personal Website", "Worklog"};
+        mProjects = new ArrayList<>();
 
         mRecyclerView = findViewById(R.id.recyclerview_projects_list);
         mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        mAdapter = new ProjectListAdapter(projects);
+        mAdapter = new ProjectListAdapter(mProjects);
 
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mProjectsRef.addValueEventListener(mOnProjectsChangeListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mProjectsRef.removeEventListener(mOnProjectsChangeListener);
+    }
+
+    public class ProjectValueEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            mProjects.clear();
+
+            for (DataSnapshot projectSnapshot: dataSnapshot.getChildren()) {
+                String project = projectSnapshot.getValue(String.class);
+                mProjects.add(project);
+            }
+
+            mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.d(TAG, "Failed to read projects.", databaseError.toException());
+        }
     }
 
     public class ProjectListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         static final int VIEW_TYPE_HEADER = 0;
         static final int VIEW_TYPE_PROJECT_ENTRY = 1;
 
-        private String[] mProjects;
+        private List<String> mProjects;
 
-        ProjectListAdapter(String[] projects) {
+        ProjectListAdapter(List<String> projects) {
             mProjects = projects;
         }
 
         String getProject(int position) {
-            return mProjects[position - 1];
+            return mProjects.get(position - 1);
         }
 
         @Override
@@ -82,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return mProjects.length + 1;
+            return mProjects.size() + 1;
         }
 
         @Override
